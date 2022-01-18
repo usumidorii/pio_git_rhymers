95e67a25bca3913e871366d6c34437541f894fd4
bartek228798 <228798@edu.p.lodz.pl>
2022-01-18 21:03:03 +0100

    1.4 stale 1

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,8 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+private static final int ZERO = 0;
+int totalRejected = ZERO;
 
 	public int reportRejected() {
 		return totalRejected;
