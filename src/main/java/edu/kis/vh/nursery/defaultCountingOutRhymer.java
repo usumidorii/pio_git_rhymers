372e18245781edad8e3d384b955eefa8e90ebb49
bartek228798 <228798@edu.p.lodz.pl>
2022-01-18 21:24:57 +0100

    1.4 stale 2

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -8,7 +8,7 @@ public class defaultCountingOutRhymer {
 
 	private static final int COUNTER = -1;
 
-	private int[] NUMBERS = new int[SIZE];
+	private final int[] NUMBERS = new int[SIZE];
 
 	public int total = COUNTER;
 
